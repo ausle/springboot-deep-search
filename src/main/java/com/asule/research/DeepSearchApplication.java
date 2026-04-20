bf87f38c6ce99de86484dd90b30345851533e322
Asule <distinctofwang@gmail.com>
2026-04-20 09:04:20 +0800

    首次提交

---
+++ src/main/java/com/asule/research/DeepSearchApplication.java
@@ -0,0 +1,13 @@
+package com.asule.research;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(DeepSearchApplication.class);
+        springApplication.run(args);
+    }
+
+}
